55ce4bfd155cf41dfb7f51de8ee38bf070c77537
Abishanan Sriranjan <abishanan.sriran@gmail.com>
2022-10-15 14:41:40 -0400

    Use body for POST requests

--- Back-End/deleteuser/views.py
+++ Back-End/deleteuser/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 import sys
 sys.path.append('../../')
 import myoutlet.db_ops.db as db
@@ -8,10 +9,12 @@ import myoutlet.db_ops.db as db
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
         # do database stuff
         try:
             collection = db.get_collection()
-            db.delete_document(collection, request.POST.__getitem__('_id'))
+            db.delete_document(collection, body['_id'])
             return JsonResponse({
                 'status': 200,
             })
--- Back-End/newuser/views.py
+++ Back-End/newuser/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 import sys
 sys.path.append('../../')
 import myoutlet.db_ops.db as db
@@ -9,18 +10,19 @@ USER_TEMPLATE = ['name', 'image', 'latitude', 'longitude', 'battery-level']
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        postData = request.POST.dict()
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
         success = True
 
         for key in USER_TEMPLATE:
-            if postData.get(key) != None:
+            if body.get(key) != None:
                 success = False
                 break
 
         if success:
             try:
                 collection = db.get_collection()
-                userid = db.insert_user_info(collection)
+                userid = db.insert_user_info(collection, json.dumps(body))
                 return JsonResponse({
                     'status': 200,
                     '_id': userid,
