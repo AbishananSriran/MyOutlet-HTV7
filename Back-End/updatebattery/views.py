46066bd981f481bb444365eb6a3b79c7c948c47c
Abishanan Sriranjan <abishanan.sriran@gmail.com>
2022-10-15 20:03:48 -0400

    Fix for updatebattery endpoint

--- Back-End/updatebattery/views.py
+++ Back-End/updatebattery/views.py
@@ -7,10 +7,13 @@ import myoutlet.db_ops.db as db
 
 @csrf_exempt
 def index(request):
+    print('ok')
     if request.method == 'POST':
+        print('ok')
         try:
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
+            print(body)
             collection = db.get_collection()
             db.update_user_battery(collection, body['_id'], body['battery-level'])
             return JsonResponse({
